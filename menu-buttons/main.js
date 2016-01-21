bf813e1fc8df6bc80c97ed623e769651cb7b1bc5
Lucilla Chalmer <chalmer.lucilla@gmail.com>
2016-01-20 20:08:13 -0800

    Finished implementation of menu items

--- menu-buttons/main.js
+++ menu-buttons/main.js
@@ -1,3 +1,4 @@
+// Custom Handlers
 function lorem () {
   console.log('lorem executed');
 }
@@ -5,3 +6,27 @@ function lorem () {
 function ipsum () {
   console.log('ipsum executed');
 }
+
+
+// Verify that DOM has loaded
+$(document).ready(
+  function () {
+    $('html').on('click', function() {
+      $('.link').not(this).removeClass('is-active');
+    })
+    $('.link').on('click', function(e) {
+      // kill the bubbling of body click event
+      e.stopPropagation();
+      $(this).toggleClass('is-active');
+      $('.link').not(this).removeClass('is-active')
+    });
+    $('li[data-js="lorem"]').on('click', function() {
+      // execute custom lorem handler
+      lorem();
+    })
+    $('li[data-js="ipsum"]').on('click', function() {
+      // execute custom ipsum hander
+      ipsum();
+    })
+  }
+);
